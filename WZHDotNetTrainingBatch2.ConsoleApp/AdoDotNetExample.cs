52d8f354695c50be5100364a3614f72061d410b4
Wai Zin Htun <waizinhtun.tze@gmail.com>
2025-06-19 15:13:18 +0630

    feat: ADO.NetCRUD

---
+++ WZHDotNetTrainingBatch2.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WZHDotNetTrainingBatch2.ConsoleApp
+{
+    public class AdoDotNetExample
+    {// we can use any methods this one  such CRUD
+
+        // press ctrl r,r to rename
+        SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetTrainingBatch2",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+        public void Read()
+        {
+            // SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+            // sqlConnectionStringBuilder.DataSource = "DESKTOP-NFOPO25";
+            //sqlConnectionStringBuilder.DataSource = "(local";
+            //sqlConnectionStringBuilder.DataSource = "Desktop name";
+            //sqlConnectionStringBuilder.InitialCatalog = "DotNetTrainingBatch2";
+            //sqlConnectionStringBuilder.UserID = "sa";
+            //sqlConnectionStringBuilder.Password = "sasa@123";
+            //sqlConnectionStringBuilder.TrustServerCertificate = true;
+
+
+
+
+            //can write two ways but inside doubleqoute we can write anything  it is not best practice
+            //SqlConnection connection = new SqlConnection("Data Source =.; Initial Catalog = DotNetTrainingBatch2; User ID = sa; Password = sasa@123");
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            Console.WriteLine("connection is opening....");
+            connection.Open();
+            Console.WriteLine("connection opened!");
+
+            string query = "select * from Tbl_Blog";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            Console.WriteLine("connection is closing...");
+            connection.Close(); // can cause connection time out that's why we need to close
+            Console.WriteLine("connection is closed!....");
+
+            //DataSet one or more tables can store
+            //DataTable 
+            //Data Row
+            //Data Column
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow row = dt.Rows[i];
+                Console.WriteLine(i);
+                Console.WriteLine("blogid =>"+ row["BlogId"]);// we don't use index can cause place error
+                Console.WriteLine("blogtitle =>" + row["BlogTitle"]);
+                Console.WriteLine("blogauthor =>" + row["BlogAuthor"]);
+                Console.WriteLine("blogcontent =>" + row["BlogContent"]);
+            }
+        }
+
+
+        public void Create()
+        {
+            Console.Write("Enter Title:");
+            string title = Console.ReadLine()!;
+
+            Console.Write("Enter Author:");
+            string author = Console.ReadLine()!;
+
+            Console.Write("Enter Content:");
+            string content = Console.ReadLine()!;
+
+            string query = $@"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           ('{title}'
+           ,'{author}'
+           ,'{content}')";
+
+
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+            Console.WriteLine(result > 0 ? "insert successed" : "insert failed");
+        
+        
+        }
+    }
+}
--- WZHDotNetTrainingBatch2.ConsoleApp/Program.cs
+++ WZHDotNetTrainingBatch2.ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.SqlTypes;
+using WZHDotNetTrainingBatch2.ConsoleApp;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 Console.WriteLine("Hello, World!");
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Read();
+//adoDotNetExample.Create();
+
+Console.ReadKey();
+
+//ADO.NET
+//Dapper
+//EF Core
