ed44212596246c02e23b142bc8ceef2e05a625ef
WaiZin <WaiZin@DESKTOP-PC92E8F>
2025-06-25 21:40:03 +0630

    feat: EfCoreDataBaseFirst

--- WZHDotNetTrainingBatch2.ConsoleApp/Program.cs
+++ WZHDotNetTrainingBatch2.ConsoleApp/Program.cs
@@ -1,11 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using WZHDotNetTrainingBatch2.ConsoleApp;
+using WZHDotNetTrainingBatch2.Database;
 
 Console.WriteLine("Hello, World!");
 EfCoreExample efCoreExample = new EfCoreExample();
 //efCoreExample.Read();
 //efCoreExample.Edit();
-efCoreExample.Update();
+//efCoreExample.Update();
 //efCoreExample.Delete(); 
+
+
 
 Console.ReadLine();
---
+++ WZHDotNetTrainingBatch2.Database/AppDbContextModels/AppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WZHDotNetTrainingBatch2.Database.AppDbContextModels;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblBlog> TblBlogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.;Database=DotNetTrainingBatch2;User ID=sa;Password=sa@@123;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblBlog>(entity =>
+        {
+            entity.HasKey(e => e.BlogId);
+
+            entity.ToTable("Tbl_Blog");
+
+            entity.Property(e => e.BlogAuthor)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.BlogContext)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.BlogTitle)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
